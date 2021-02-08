517fbbcd9f998128383761002e3aabcb3e58db7d
Ewan Brain <ewan.brain@gmail.com>
2021-02-08 16:50:04 +0000

    MVP done

--- src/main/java/WaterBottle.java
+++ src/main/java/WaterBottle.java
@@ -9,4 +9,12 @@ public class WaterBottle {
     public int drink(){
         return volume - 10;
     }
+
+    public int empty() {
+        return volume = 0;
+    }
+
+    public int fill() {
+        return volume = 100;
+    }
 }
--- src/test/java/WaterBottleTest.java
+++ src/test/java/WaterBottleTest.java
@@ -1,2 +1,32 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class WaterBottleTest {
+
+    WaterBottle waterBottle;
+
+    @Before
+    public void before() {
+        waterBottle = new WaterBottle(100);
+    }
+
+
+
+    @Test
+    public void canDrink() {
+        assertEquals(90, waterBottle.drink());
+    }
+
+    @Test
+    public void canEmpty() {
+        assertEquals(0, waterBottle.empty());
+    }
+
+    @Test
+    public void canFill() {
+        WaterBottle emptyBottle = new WaterBottle(0);
+        assertEquals(100, emptyBottle.fill());
+    }
 }
