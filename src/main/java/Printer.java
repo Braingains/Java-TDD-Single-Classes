81138d590a756cddde6085480e24dfe32a345ac0
Ewan Brain <ewan.brain@gmail.com>
2021-02-08 18:10:15 +0000

    print function refactored, tests passing, extensions complete

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,20 +1,28 @@
 public class Printer {
 
     private int numberOfRemainingSheets;
+    private int remainingToner;
 
-    public Printer(int numberOfRemainingSheets){
+    public Printer(int numberOfRemainingSheets, int remainingToner){
         this.numberOfRemainingSheets = numberOfRemainingSheets;
+        this.remainingToner = remainingToner;
 
     }
 
-    public int print(int pages, int copies){
+    public boolean print(int pages, int copies){
         int paperRequired = pages * copies;
-        if (paperRequired <= numberOfRemainingSheets){
-            return numberOfRemainingSheets - paperRequired;
+        if ((paperRequired <= numberOfRemainingSheets) & (paperRequired <= remainingToner)) {
+            numberOfRemainingSheets -= paperRequired;
+            remainingToner -= paperRequired;
+            return true;
         } else {
-            return 0;
+            return false;
         }
     }
+
+    public int getRemainingSheets(){ return this.numberOfRemainingSheets; }
+
+    public int getRemainingToner(){ return this.remainingToner; }
 
 }
 
--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -8,13 +8,50 @@ public class PrinterTest {
     Printer printer;
 
     @Before
-    public void before() { printer = new Printer(100);
+    public void before() { printer = new Printer(100, 1000);
     }
 
 
+
+    
+    @Test
+    public void printerHasSheets() {
+        assertEquals(100, printer.getRemainingSheets());
+    }
+
+    @Test
+    public void printerHasToner() {
+        assertEquals(1000, printer.getRemainingToner());
+    }
 
     @Test
     public void canPrintTwoPagesFiveCopies() {
-        assertEquals(90, printer.print(2,5));
+        assertEquals(true, printer.print(2,5));
     }
+
+    @Test
+    public void cannotPrintTwoPagesSixtyCopies() {
+        assertEquals(false, printer.print(2, 60));
+    }
+
+    @Test
+    public void cannotPrintWithoutToner() {
+        Printer lowToner = new Printer (100, 2);
+        assertEquals(false, lowToner.print(3, 10));
+    }
+
+    @Test
+    public void paperIsUsed() {
+        printer.print(2, 5);
+        assertEquals(90, printer.getRemainingSheets());
+    }
+
+    @Test
+    public void tonerIsUsed() {
+        printer.print(2, 5);
+        assertEquals(990, printer.getRemainingToner());
+    }
+
+
+
 }
